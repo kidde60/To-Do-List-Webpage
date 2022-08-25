ad503a6fbf68115e4bd499ee6da9bd35b0c48d45
kidde60 <kgeorgewilliam60@gmail.com>
2022-08-25 15:30:39 -0700

    Add test to update index

--- __tests__/add-and-remove.test.js
+++ __tests__/add-and-remove.test.js
@@ -33,11 +33,11 @@ describe('remove a task from the list', () => {
     expect(localstorage.getAllItems()[0].description).toBe('Task 2');
   });
 
-  /* test('should have a length of 1 after removing Task 1 from the list in the DOM', () => {
+   test('should have a length of 1 after removing Task 1 from the list in the DOM', () => {
     tasksDisplay();
 
     const listContainer = Array.from(document.querySelector('.list').childNodes);
     
     expect(listContainer.length).toBe(1);
-  }); */
+  }); 
 });
